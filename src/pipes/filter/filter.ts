7f4212e15b6fc990418a8a91ad3baa573c134ca2
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-02-01 15:44:03 +0700

    update

--- src/pipes/filter/filter.ts
+++ src/pipes/filter/filter.ts
@@ -10,16 +10,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(list: any[], obj: any): any[] {
-    if (obj) {
+    if (obj && list[0][Object.keys(obj)[0]]) {
       let key = Object.keys(obj);
-      if (key) {
-        let val = obj[key[0]].toUpperCase();
-        return list.filter(item => {
-          return item[key[0]].toUpperCase().indexOf(val) !== -1
-        });
-      } else {
-        return list;
-      }
+      let val = obj[key[0]].toUpperCase();
+      return list.filter(item => {
+        return item[key[0]].toUpperCase().indexOf(val) !== -1
+      });
     } else {
       return list;
     }
