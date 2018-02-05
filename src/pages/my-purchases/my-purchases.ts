0ea84dc026c4dc41a181de84040eedae3ca65a4c
amonratchantawon <amonrat.cha@northbkk.ac.th>
2018-02-05 17:58:27 +0700

    sdaasdasd

--- src/pages/my-purchases/my-purchases.ts
+++ src/pages/my-purchases/my-purchases.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 
 /**
  * Generated class for the MyPurchasesPage page.
@@ -14,12 +14,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'my-purchases.html',
 })
 export class MyPurchasesPage {
-
+  @ViewChild('pageSlider') pageSlider: Slides;
+  @ViewChild('pageNav') pageNav: Slides;
+  public tabs: any = '0';
+  pages: string = '0';
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyPurchasesPage');
+  }
+
+  selectTab(index) {
+    this.pageSlider.slideTo(index);
+    this.pageNav.slideTo(index);
+  }
+
+  changeWillSlide($event) {
+    // this.tabs = $event._snapIndex.toString();
+    this.tabs = this.pages;
+    let page = parseInt(this.tabs)
+    let pageNumber = $event._snapIndex;
+    if (page < pageNumber) {
+      page++;
+      this.pages = page.toString();
+      let scroll = document.getElementById('scroll');
+      scroll.scrollLeft = 100 * page;
+    }
   }
 
 }
