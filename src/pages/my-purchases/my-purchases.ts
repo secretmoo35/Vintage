dcbab79be6567447c82c6296c79d875e33144183
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-02-05 23:02:10 +0700

    update tab

--- src/pages/my-purchases/my-purchases.ts
+++ src/pages/my-purchases/my-purchases.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Slides, Platform, Content } from 'ionic-angular';
 
 /**
  * Generated class for the MyPurchasesPage page.
@@ -14,60 +14,160 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
   templateUrl: 'my-purchases.html',
 })
 export class MyPurchasesPage {
-  @ViewChild('pageSlider') pageSlider: Slides;
-  tabs: any = '0';
+  // @ViewChild('pageSlider') pageSlider: Slides;
+  // tabs: any = '0';
+  // constructor(
+  //   public navCtrl: NavController,
+  //   public navParams: NavParams
+  // ) {
+
+  // }
+
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad MyPurchasesPage');
+  // }
+
+  // selectTab(index) {
+  //   this.pageSlider.slideTo(index);
+  // }
+
+  // ionSlidePrevStart($event) {
+  //   let page = parseInt(this.tabs)
+  //   let pageNumber = $event._snapIndex;
+  //   if (page > pageNumber) {
+  //     this.tabs = pageNumber.toString();
+  //     let scroll = document.getElementById('scroll');
+  //     page--;
+  //     let sum = 100 * page;
+  //     let o_scrollLeft = 0;
+  //     let delay = setInterval(() => {
+  //       o_scrollLeft = scroll.scrollLeft;
+  //       scroll.scrollLeft -= 5;
+  //       if (scroll.scrollLeft <= sum || scroll.scrollLeft === o_scrollLeft) {
+  //         clearInterval(delay);
+  //       }
+  //     }, 10);
+  //   }
+  // }
+
+  // ionSlideNextStart($event) {
+  //   let page = parseInt(this.tabs)
+  //   let pageNumber = $event._snapIndex;
+  //   if (page < pageNumber) {
+  //     this.tabs = pageNumber.toString();
+  //     let scroll = document.getElementById('scroll');
+  //     page++;
+  //     let sum = 100 * page;
+  //     let o_scrollLeft = 0;
+  //     scroll.scrollLeft = sum - 100;
+  //     let delay = setInterval(() => {
+  //       o_scrollLeft = scroll.scrollLeft;
+  //       scroll.scrollLeft += 5;
+  //       if (scroll.scrollLeft === sum || scroll.scrollLeft === o_scrollLeft) {
+  //         clearInterval(delay);
+  //       }
+  //     }, 10);
+  //   }
+  // }
+  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides;
+  @ViewChild('MultiItemsScrollingTabs') ItemsTitles: Content;
+
+  SwipedTabsIndicator: any = null;
+  tabTitleWidthArray: any = [];
+  tabElementWidth_px: number = 50;
+  screenWidth_px: number = 0;
+  isRight: boolean = true;
+  isLeft: boolean = true;
+  tabs: any = [];
+
+
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private platform: Platform
   ) {
+    this.tabs = ["page0", "1", "page2", "3", "page4", "5", "page6", "7", "page8", "9", "page For tutorual"];
+    console.log('Width: ' + platform.width());
+    this.screenWidth_px = platform.width();
 
   }
+  ionViewDidEnter() {
+    this.SwipedTabsIndicator = document.getElementById("indicator");
+    for (let i in this.tabs)
+      this.tabTitleWidthArray.push(document.getElementById("tabTitle" + i).offsetWidth);
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MyPurchasesPage');
+    this.selectTab(0);
+  }
+
+  scrollIndicatiorTab() {
+    this.ItemsTitles.scrollTo(this.calculateDistanceToSpnd(this.SwipedTabsSlider.getActiveIndex()) - this.screenWidth_px / 2, 0);
   }
 
   selectTab(index) {
-    this.pageSlider.slideTo(index);
+    this.SwipedTabsIndicator.style.width = this.tabTitleWidthArray[index] + "px";
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (this.calculateDistanceToSpnd(index)) + 'px,0,0)';
+    this.SwipedTabsSlider.slideTo(index);
   }
 
-  ionSlidePrevStart($event) {
-    let page = parseInt(this.tabs)
-    let pageNumber = $event._snapIndex;
-    if (page > pageNumber) {
-      this.tabs = pageNumber.toString();
-      let scroll = document.getElementById('scroll');
-      page--;
-      let sum = 100 * page;
-      let o_scrollLeft = 0;
-      let delay = setInterval(() => {
-        o_scrollLeft = scroll.scrollLeft;
-        scroll.scrollLeft -= 5;
-        if (scroll.scrollLeft <= sum || scroll.scrollLeft === o_scrollLeft) {
-          clearInterval(delay);
-        }
-      }, 10);
+  calculateDistanceToSpnd(index) {
+    var result = 0;
+    for (var _i = 0; _i < index; _i++) {
+      result = result + this.tabTitleWidthArray[_i];
     }
+    return result;
   }
 
-  ionSlideNextStart($event) {
-    let page = parseInt(this.tabs)
-    let pageNumber = $event._snapIndex;
-    if (page < pageNumber) {
-      this.tabs = pageNumber.toString();
-      let scroll = document.getElementById('scroll');
-      page++;
-      let sum = 100 * page;
-      let o_scrollLeft = 0;
-      scroll.scrollLeft = sum - 100;
-      let delay = setInterval(() => {
-        o_scrollLeft = scroll.scrollLeft;
-        scroll.scrollLeft += 5;
-        if (scroll.scrollLeft === sum || scroll.scrollLeft === o_scrollLeft) {
-          clearInterval(delay);
-        }
-      }, 10);
-    }
+  updateIndicatorPosition() {
+    var index = this.SwipedTabsSlider.getActiveIndex();
+    if (this.SwipedTabsSlider.length() == index)
+      index = index - 1;
+
+    this.SwipedTabsIndicator.style.width = this.tabTitleWidthArray[index] + "px";
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (this.calculateDistanceToSpnd(index)) + 'px,0,0)';
+
   }
+
+  updateIndicatorPositionOnTouchEnd() {
+    setTimeout(() => { this.updateIndicatorPosition(); }, 200);
+  }
+
+  animateIndicator($event) {
+
+    this.isLeft = false;
+    this.isRight = false;
+    var currentSliderCenterProgress = (1 / (this.SwipedTabsSlider.length() - 1)) * this.SwipedTabsSlider.getActiveIndex();
+    if ($event.progress < currentSliderCenterProgress) {
+      this.isLeft = true;
+      this.isRight = false;
+
+    } if ($event.progress > currentSliderCenterProgress) {
+      this.isLeft = false;
+      this.isRight = true;
+    }
+
+    if (this.SwipedTabsSlider.isEnd())
+      this.isRight = false;
+
+    if (this.SwipedTabsSlider.isBeginning())
+      this.isLeft = false;
+
+    if (this.isRight)
+      this.SwipedTabsIndicator.style.webkitTransform =
+        'translate3d(' + (this.calculateDistanceToSpnd(this.SwipedTabsSlider.getActiveIndex())
+          + ($event.progress - currentSliderCenterProgress) * (this.SwipedTabsSlider.length() - 1) * this.tabTitleWidthArray[this.SwipedTabsSlider.getActiveIndex() + 1])
+        + 'px,0,0)';
+
+    if (this.isLeft)
+      this.SwipedTabsIndicator.style.webkitTransform =
+        'translate3d(' + (this.calculateDistanceToSpnd(this.SwipedTabsSlider.getActiveIndex())
+          + ($event.progress - currentSliderCenterProgress) * (this.SwipedTabsSlider.length() - 1) * this.tabTitleWidthArray[this.SwipedTabsSlider.getActiveIndex() - 1])
+        + 'px,0,0)';
+
+    if (!this.isRight && !this.isLeft)
+      this.SwipedTabsIndicator.style.width = this.tabTitleWidthArray[this.SwipedTabsSlider.getActiveIndex()] + "px";
+
+  }
+
+
 
 }
