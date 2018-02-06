1c754cc51b2dc5a83aae8d0a12e5f6bcc6e50de9
amonratchantawon <amonrat.cha@northbkk.ac.th>
2018-02-06 12:14:20 +0700

    pay ment

--- src/pages/step-order/step-order.module.ts
+++ src/pages/step-order/step-order.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StepOrderPage } from './step-order';
+import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +10,8 @@ import { StepOrderPage } from './step-order';
   ],
   imports: [
     IonicPageModule.forChild(StepOrderPage),
+    TranslateModule.forChild(),
+    ComponentsModule
   ],
 })
 export class StepOrderPageModule {}
